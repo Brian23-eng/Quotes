1b14ac6722fd439b58d204bab3003233534f482c
brian23-eng <b.odhiambo.bo@gmail.com>
2019-09-20 23:04:54 +0300

    create the 'this' instance in the constratctor for instatiate the class and creat the ngFor derective to loop over the array of id and name, create an interpolation binding og id and name to view them on the server

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,14 +9,16 @@ import { Quotes} from '../quotes'
 })
 export class QuotesComponent implements OnInit {
 
-  quotes:Quotes [] = [
-    {id:1, name:'Life is what happens when you are busy making other plans'},
-    {id:2, name:'Be happy for this moment. This Moment is your life'},
-    {id:3, name:'I think being in love with life is a key to eternal youth'},
-    {id:4, name:'My mission in life is not merely to survive, but to thrive; and to do so with some passion,some compassion, some humor and some style'}
-  ]
+  quotes:Quotes []
 
-  constructor() { }
+  constructor() { 
+   this.quotes = [
+      {id:1, name:'Life is what happens when you are busy making other plans'},
+      {id:2, name:'Be happy for this moment. This Moment is your life'},
+      {id:3, name:'I think being in love with life is a key to eternal youth'},
+      {id:4, name:'My mission in life is not merely to survive, but to thrive; and to do so with some passion,some compassion, some humor and some style'}
+    ]
+  }
 
   ngOnInit() {
   }
