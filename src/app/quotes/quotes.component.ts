68b423838aef12899661d14934da6907630ba835
brian23-eng <b.odhiambo.bo@gmail.com>
2019-09-20 22:48:28 +0300

    generate a class, create an array id with type number and name with type string then import the class to quotes component ts file where i export my id and name in the class to be accessible outside

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes} from '../quotes'
+
 
 @Component({
   selector: 'app-quotes',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-  quote = ''
+  quotes:Quotes [] = [
+    {id:1, name:'Life is what happens when you are busy making other plans'},
+    {id:2, name:'Be happy for this moment. This Moment is your life'},
+    {id:3, name:'I think being in love with life is a key to eternal youth'},
+    {id:4, name:'My mission in life is not merely to survive, but to thrive; and to do so with some passion,some compassion, some humor and some style'}
+  ]
 
   constructor() { }
 
